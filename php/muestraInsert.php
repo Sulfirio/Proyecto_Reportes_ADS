77eecef1a5a32e3b7e3e1d1c1e29fc01af2de035
mrRaindrop <chrismre@hotmail.com>
2023-05-28 20:12:56 -0600

    un mensaje

---
+++ php/muestraInsert.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<?php
+                include 'conexion.php';
+                $reportes = $conexion->query("SELECT * FROM reporte");
+                $a;
+
+                while($usuarios = mysqli_fetch_array($reportes)){
+                    $a = $usuarios['folio'];
+                }
+                $a = $a+1;
+                echo $a;
+                $b = "";
+                $c = "";
+                $d = 0;
+                $sql = $conexion->query("INSERT INTO reporte (folio, nivelUrg, idCapturador, aprobado) VALUES ($a,$b,$c,$d)");
+                $exito = mysqli_query($conexion, $sql);
+                if($exito){
+                    echo "Logrado";
+                }else{
+                    echo "Ocurrio un error";
+                }
+            ?>
