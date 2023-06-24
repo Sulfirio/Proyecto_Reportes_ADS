fe6bf775d73fe5fc6ec7486810aa7336ccf7c2f2
Akira <diegocasarosa@hotmail.com>
2023-06-24 00:58:54 -0600

    Formulario_HARD terminado?-Alojado en InicioU3.html

--- js/funCaptura.js
+++ js/funCaptura.js
@@ -1,4 +1,5 @@
-var selected = []; // Variable para almacenar las claves seleccionadas
+var selectedKeys = []; // Arreglo para almacenar las claves seleccionadas
+var selectedValues = []; // Arreglo para almacenar los valores correspondientes
 
 $.ajax({
     url: 'php/Formulario_HARD.php',
@@ -12,40 +13,92 @@ $.ajax({
 
             var box = document.createElement('div');
             box.className = 'box';
-            var checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
-            checkbox.value = clave;
-            checkbox.addEventListener('change', function(){
-                // Verifica si la casilla ha sido seleccionada
-                if(this.checked){
-                    // Si es así, agrega la clave al array
-                    selected.push(this.value);
-                } else {
-                    // Si no, elimina la clave del array
-                    var index = selected.indexOf(this.value);
-                    if(index > -1){
-                        selected.splice(index, 1);
+
+            // Creamos un campo de entrada de números en lugar de una casilla de verificación
+            var numberInput = document.createElement('input');
+            numberInput.type = 'number';
+            numberInput.placeholder = '0'; // usamos placeholder en lugar de value
+            numberInput.addEventListener('change', (function(clave){
+                return function(){
+                    // Si el campo se deja vacío, se vuelve a establecer a '0'
+                    if(this.value === ''){
+                        this.value = '0';
+                    }
+
+                    // Verificamos si el valor ha cambiado
+                    if(this.value != 0){
+                        // Si es así, agregamos el valor y la clave a los respectivos arreglos
+                        selectedKeys.push(clave);
+                        selectedValues.push(this.value);
+                    } else {
+                        // Si no, eliminamos la clave y el valor de los respectivos arreglos
+                        for(var j = 0; j < selectedKeys.length; j++) {
+                            if(selectedKeys[j] == clave) {
+                                selectedKeys.splice(j, 1);
+                                selectedValues.splice(j, 1);
+                            }
+                        }
                     }
                 }
-            });
+            })(clave));
+            
             var text = document.createTextNode(clave + " / " + nombre);
             box.appendChild(text);
-            box.appendChild(checkbox);
+            box.appendChild(numberInput);
             document.getElementById('container').appendChild(box);
         }
     }
 });
 
-// Mostrar claves seleccionadas al hacer clic en el botón
 document.getElementById('showSelected').addEventListener('click', function(){
-    alert(selected.join(', '));
+    var keysOutput = "Claves:\n";
+    var valuesOutput = "Valores:\n";
+    
+    for(var i = 0; i < selectedKeys.length; i++) {
+        keysOutput += selectedKeys[i] + "\t";
+        valuesOutput += selectedValues[i] + "\t";
+    }
+    
+    alert(keysOutput + "\n\n" + valuesOutput);
 });
 
-// Manejador de eventos para el cuadro de entrada para el primer cuadro de texto!!!
+/*/ Recoger el valor de cada textarea
+var razonSocial = document.getElementById('userInput0').value;
+var justificacion = document.getElementById('userInput1').value;
+
+//Recoger el valor del Mes
+var mesSeleccionado = document.getElementById('meses').value;
+*/
+
+// Enviando los datos a otro archivo PHP
+document.getElementById('sendData').addEventListener('click', function(){
+    $.ajax({
+        url: 'php/Insert_Form_HARD.php', // Actualiza esto a la URL de tu script PHP
+        type: 'post',
+        data: {
+            keys: JSON.stringify(selectedKeys),
+            values: JSON.stringify(selectedValues),
+            razonSocial: document.getElementById('userInput0').value,
+            justificacion: document.getElementById('userInput1').value,
+            mes: document.getElementById('meses').value,
+        },
+        success: function(response){
+            // Esto se ejecuta cuando el servidor responde exitosamente
+            console.log(response);
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            // Esto se ejecuta si ocurre un error
+            console.error(textStatus, errorThrown);
+        }
+    });
+});
+
+
+/*/ Manejador de eventos para el cuadro de entrada para el primer cuadro de texto
 document.getElementById('userInput0').addEventListener('input', function(){
     console.log('El usuario escribió en 0: ' + this.value);
 });
 
 document.getElementById('userInput1').addEventListener('input', function(){
     console.log('El usuario escribió en 1: ' + this.value);
-});
+});*/
---
+++ php/Insert_Form_HARD.php
@@ -0,0 +1,33 @@
+<?php
+include 'conexion.php';
+if(isset($_POST['keys']) && isset($_POST['values']) && isset($_POST['razonSocial']) && isset($_POST['justificacion'])) {
+    $keys = json_decode($_POST['keys']);
+    $values = json_decode($_POST['values']);
+    $razonSocial = $_POST['razonSocial'];
+    $justificacion = $_POST['justificacion'];
+    $mes = $_POST['mes'];
+
+    $num = 1;
+    $datos = $conexion->query("SELECT * FROM estimacion");
+    while($row = mysqli_fetch_array($datos)){
+        $num = $row['idEstimacion'];
+    }
+    $num = $num+1;
+
+    for($i = 0;  $i < count($values); $i++){
+
+        $concepto = $conexion->query("SELECT * FROM conceptos where clave = {$keys[$i]}");
+        while($data = mysqli_fetch_array($concepto)){
+            $coste = $data['importe'];
+        }
+        $importe = $values[$i] * $coste;
+        $sql = "INSERT INTO estimacion (idEstimacion, claveConcepto,
+                            idEstimador, noFrenteObra, razSoc,
+                            justificacion, mes, cantidad, importe,nvAprobacion) 
+                            VALUES ($num,$keys[$i],'UsuarioA',1,'$razonSocial', '$justificacion','$mes',
+                            $values[$i],$importe,1)";
+
+        $exito = mysqli_query($conexion, $sql);
+    }
+}
+?>
