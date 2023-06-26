286abb5e5ff897a0b38586b88c1746663e28833d
JVRALPH <73514284+JVRALPH@users.noreply.github.com>
2023-06-25 22:38:47 -0600

    Delete login.js

--- login/js/login.js
+++
@@ -1,8 +0,0 @@
-const userid = document.getElementById("inputEmail")
-const userpassword = document.getElementById("inputPassword")
-const btn = document.getElementById("btnlog")
-
-btn.addEventListener("click",function(event){
-    if(userid.value == "" && userpassword.value == ""){
-    alert("Ingresa usuario y contraseña")}
-})
