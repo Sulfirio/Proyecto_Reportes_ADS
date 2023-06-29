013e2fd15188cbdfd063daac8499c4f150af5791
JVRALPH <73514284+JVRALPH@users.noreply.github.com>
2023-06-28 22:35:21 -0600

    Login

--- login/login.php
+++ login/login.php
@@ -19,30 +19,32 @@
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
                                     <div class="card-header"><h3 class="text-center font-weight-light my-4">Iniciar sesión</h3></div>
                                     <div class="card-body">
+                                    <?php
+                                            if (isset($_GET['error']) && $_GET['error'] == 1) {
+                                                echo '<div class="alert alert-danger">Usuario o contraseña incorrectos. Por favor, intenta nuevamente.</div>';
+                                            }
+                                            ?>
                                         <form action="loginP.php" method="POST">
                                             <div class="form-floating mb-3">
-                                                <input class="form-control" id="inputEmail" name="inputEmail" type="" placeholder="name@example.com" />
+                                                <input class="form-control" id="inputEmail" name="inputEmail" type="" placeholder="name@example.com o userName" />
                                                 <label for="inputEmail">Correo</label>
                                             </div>
                                             <div class="form-floating mb-3">
                                                 <input class="form-control" id="inputPassword" name="inputPassword" type="password" placeholder="Password" />
                                                 <label for="inputPassword">Contraseña</label>
                                             </div>
+                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                            </div>
                                             <div class="form-check mb-3">
-                                                <input class="form-check-input" id="inputRememberPassword" name="inputRememberPassword" type="checkbox" value="" />
-                                                <label class="form-check-label" for="inputRememberPassword">Remember Password</label>
+                                                <input class="form-check-input" id="inputRememberPassword" name="inputRememberPassword" type="checkbox" value="" <?php echo isset($_COOKIE['loginEmail']) ? 'checked' : ''; ?> />
+                                                <label class="form-check-label" for="inputRememberPassword">Recordar contraseña</label>
                                             </div>
                                             <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                                <a class="small" href="password.html">Olvidaste la contraseña?</a>
                                             </div>
-                                            <div>
                                                 <div class="d-flex align-items-center justify-content-between mt-3 mb-0"></div>
                                                 <button type="submit" id="btnlog" class="btn btn-primary">Entrar</a>
                                             </div>
                                         </form>
-                                    </div>
-                                    <div class="card-footer text-center py-3">
-                                        <div class="small"><a href="register.html">Registro</a></div>
                                     </div>
                                 </div>
                             </div>
@@ -51,6 +53,5 @@
                 </main>
             </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/login.js"></script>
     </body>
 </html>
